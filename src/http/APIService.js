00219a10b973c711ee4846e1f0467782d15e0934
pkandel1 <pkandel@unomaha.edu>
2022-04-05 21:09:41 -0500

    Update APIService.js

--- src/http/APIService.js
+++ src/http/APIService.js
@@ -1,7 +1,7 @@
  /* eslint-disable */
  import axios from 'axios';
  //const API_URL = 'https://akanley.pythonanywhere.com';
- const API_URL = 'http://pkandel122.pythonanywhere.com/';
+ const API_URL = 'http://pkandel122.pythonanywhere.com';
  
  export class APIService {
    constructor() {
